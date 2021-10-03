3a175d1fdab272a2833b15af2a08c823012dac43
rafaelapcruz <79165115+rafaelapcruz@users.noreply.github.com>
2021-10-03 12:17:19 +0100

    Update main.js
    removeu "Animation on scroll". Não interfere com o jogo. AOS não está importado nos scripts dos ficheiros HTML.

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,16 +134,3 @@
       }
     }
   })
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  })
-})()
